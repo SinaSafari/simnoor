cfc76aff7ca91283031896837b7960c308f21796
Sina Safari <m.sinasafari@gmail.com>
2019-11-30 13:01:14 +0330

    add failed type

--- Redux/reducers/testReducer.js
+++ Redux/reducers/testReducer.js
@@ -1,4 +1,4 @@
-import { TEST_SUCCESS } from '../constants/testConstants'
+import { TEST_SUCCESS, TEST_FAILED } from '../constants/testConstants'
 import { createReducer } from '../../common/util/reducerUtil'
 
 const initialState = {};
@@ -8,7 +8,12 @@ export const testSuccess = (state, payload) => {
   return { ...state, message: payload }
 }
 
+export const testFail = (state, payload) => {
+  return { ...state, message: payload }
+}
+
 
 export default createReducer(initialState, {
   [TEST_SUCCESS]: testSuccess,
+  [TEST_FAILED]: testFail,
 })
