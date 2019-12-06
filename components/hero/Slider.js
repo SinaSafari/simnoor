83f929d0d1547a9dc2200f352162de7f1ae3031a
Sina Safari <m.sinasafari@gmail.com>
2019-12-06 13:27:11 +0330

    positioning the h1 and btn in slider compoent

--- components/hero/Slider.js
+++ components/hero/Slider.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import ImageWrapper from '../ImageWrapper'
 
+import Botton from '../Button/Button'
 
-
-function Slider({ img, title }) {
+function Slider({ img, title, btnTitle }) {
     return (
         <div>
             <div className='card'>
                 <ImageWrapper iconName={img} className='card-img-top' />
-
-                <div className='card-img-overlay'>
-                    <p>{title}e</p>
+                {/* style={{ marginTop: '100px' }} */}
+                <div className='card-img-overlay container ' >
+                    <h1 className='text-center' style={{ color: 'white' }}><span style={{ background: '#ffc001', padding: '0px 10px' }}>{title}</span></h1>
+                    <div style={{ marginTop: '30px' }}>
+                        <Botton title={btnTitle} classes='d-flex justify-content-center' />
+                    </div>
                 </div>
             </div>
+            <style jsx>{`
+                
+            `}
+            </style>
         </div >
     )
 }
