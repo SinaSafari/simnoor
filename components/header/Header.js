3636c4a2ca9b80e018869915c0974ee2279a2024
Sina Safari <m.sinasafari@gmail.com>
2019-12-17 16:15:32 +0330

    active link in header

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Link from 'next/link'
+import MeanuItem from './MenuItem'
 
 import { Nav, Navbar } from 'react-bootstrap'
 import ImageWrapper from '../ImageWrapper'
@@ -12,84 +12,17 @@ const COMPANY_DETAILS = {
     subAddress: 'Tehran,Iran'
 }
 
-
-const MENU_ITEMS = [
-    {
-        id: 1,
-        title: 'Home',
-        route: '/'
-    },
-    {
-        id: 2,
-        title: 'About Us',
-        route: '/about'
-
-    },
-    {
-        id: 3,
-        title: 'Solutions',
-        route: '/solutions'
-    },
-    {
-        id: 4,
-        title: 'Contact',
-        route: '/contact'
-    },
+const MENU = [
+    { id: 1, pathTo: '/', routeName: 'Home' },
+    { id: 2, pathTo: '/about', routeName: 'About' },
+    { id: 3, pathTo: '/solutions', routeName: 'Solutions' },
+    { id: 4, pathTo: '/contact', routeName: 'Contact' }
 ]
 
 
 function Header() {
+
     return (
-        // <header className="header-section clearfix">
-        //     <div className="site-navbar">
-        //         <a href="" className="site-logo">
-        //             <ImageWrapper iconName='logo' />
-        //         </a>
-        //         <div className="header-right">
-        //             <div className="header-info-box">
-        //                 <div className="hib-icon">
-        //                     <ImageWrapper iconName='phone' />
-        //                 </div>
-        //                 <div className="hib-text">
-        //                     {/* <h6>+546 990221 123</h6> */}
-        //                     <h6>{COMPANY_DETAILS.phoneNumber}</h6>
-
-        //                     {/* <p>contact@industryalinc.com</p> */}
-        //                     <p>{COMPANY_DETAILS.email}</p>
-        //                 </div>
-        //             </div>
-        //             <div className="header-info-box">
-        //                 <div className="hib-icon">
-        //                     <ImageWrapper iconName='mapMarker' />
-        //                 </div>
-        //                 <div className="hib-text">
-        //                     {/* <h6>Main Str, no 23</h6> */}
-        //                     <h6>{COMPANY_DETAILS.mainAddress}</h6>
-        //                     {/* <p>NY, New York PK 23589</p> */}
-        //                     <p>{COMPANY_DETAILS.subAddress}</p>
-        //                 </div>
-        //             </div>
-        //             <button className="search-switch">{/*<ImageWrapper iconName='search' /> */}</button>
-        //         </div>
-        //         <nav className="site-nav-menu">
-        //             <ul>
-
-        //                 {
-        //                     MENU_ITEMS.map(item => (
-        //                         <MenuItem key={item.id} >
-        //                             <Link href={`${item.route}`}>
-        //                                 <a>
-        //                                     {item.title}
-        //                                 </a>
-        //                             </Link>
-        //                         </MenuItem>
-        //                     ))
-        //                 }
-        //             </ul>
-        //         </nav>
-
-        //     </div>
-        // </header>
 
 
         <Navbar bg="lihgt" expand="md">
@@ -103,10 +36,7 @@ function Header() {
                             <ImageWrapper iconName='phone' />
                         </div>
                         <div className="hib-text">
-                            {/* <h6>+546 990221 123</h6> */}
                             <h6>{COMPANY_DETAILS.phoneNumber}</h6>
-
-                            {/* <p>contact@industryalinc.com</p> */}
                             <p>{COMPANY_DETAILS.email}</p>
                         </div>
                     </div>
@@ -115,9 +45,7 @@ function Header() {
                             <ImageWrapper iconName='mapMarker' />
                         </div>
                         <div className="hib-text">
-                            {/* <h6>Main Str, no 23</h6> */}
                             <h6>{COMPANY_DETAILS.mainAddress}</h6>
-                            {/* <p>NY, New York PK 23589</p> */}
                             <p>{COMPANY_DETAILS.subAddress}</p>
                         </div>
                     </div>
@@ -127,21 +55,14 @@ function Header() {
             <Navbar.Collapse id="basic-navbar-nav" >
                 <Nav as='nav' className="mr-auto site-nav-menu">
                     <ul>
-                        <li><Link href='/' ><a>Home</a></Link></li>
-                        <li><Link href="/about"><a>About</a></Link></li>
-                        <li><Link href="/solutions"><a>Solutions</a></Link></li>
-                        <li><Link href="contact"><a>Contact Us</a></Link></li>
+                        {
+                            MENU.map(item => {
+                                return (
+                                    <MeanuItem key={item.id} pathTo={item.pathTo} routeName={item.routeName} />
+                                )
+                            })
+                        }
                     </ul>
-                    {/* <ul>
-                        <li><Nav.Link href='/' >Home</Nav.Link></li>
-                        <li><Nav.Link href="/about">About</Nav.Link></li>
-                        <li><Nav.Link href="/solutions">Solutions</Nav.Link></li>
-                        <li><Nav.Link href="contact">Contact Us</Nav.Link></li>
-                    </ul> */}
-                    {/* <Nav.Link href="#">Home</Nav.Link>
-                    <Nav.Link href="#">About</Nav.Link>
-                    <Nav.Link href="#">Solutions</Nav.Link>
-                    <Nav.Link href="#">Contact Us</Nav.Link> */}
                 </Nav>
 
             </Navbar.Collapse>
--- components/header/MenuItem.js
+++ components/header/MenuItem.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
-function MenuItem({ children }) {
+function MenuItem({ routeName, pathTo }) {
+    const router = useRouter()
     return (
-        <li>{children}</li>
+        <>
+            <li >
+                <Link href={pathTo}>
+                    <a className={
+                        router.pathname == pathTo ? 'selected' : ''
+                    }>
+                        {routeName}
+                    </a>
+                </Link>
+            </li>
+            <style jsx>{`
+            .selected{
+                color: #b44501;
+            }
+            
+            `}</style>
+        </>
 
     )
 }
